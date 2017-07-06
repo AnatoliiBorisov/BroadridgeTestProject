10221f414cdabaff73f8e489c1b04dd27bcd46ed
AnatoliiBorisov <borisov-anatolii@mail.ru>
2017-07-06 10:56:03 +0300

    added chart service
    added chart service

--- Scripts/angular-ui/ui-chart.js
+++ Scripts/angular-ui/ui-chart.js
@@ -1,35 +1,19 @@
 ﻿angular.module('ui.chart', [])
-  .directive('uiChart', function () {
-    return {
-      restrict: 'EACM',
-      template: '<div></div>',
-      replace: true,
-      link: function (scope, elem, attrs) {
-        var renderChart = function () {
-          var data = scope.$eval(attrs.uiChart);
-          elem.html('');
-          if (!angular.isArray(data)) {
-            return;
-          }
-
-          var opts = {};
-          if (!angular.isUndefined(attrs.chartOptions)) {
-            opts = scope.$eval(attrs.chartOptions);
-            if (!angular.isObject(opts)) {
-              throw 'Invalid ui.chart options attribute';
+    .service('chartService', function () {
+        this.drawChart = function (chartDivId, title, data, options) {
+            if (options == undefined) {
+                options = {
+                    title: title,
+                    seriesDefaults: {
+                        renderer: jQuery.jqplot.PieRenderer,
+                        rendererOptions: {
+                            showDataLabels: true
+                        }
+                    },
+                    legend: { show: true, location: 'e' }
+                };
             }
-          }
 
-          elem.jqplot(data, opts);
-        };
-
-        scope.$watch(attrs.uiChart, function () {
-          renderChart();
-        }, true);
-
-        scope.$watch(attrs.chartOptions, function () {
-          renderChart();
-        });
-      }
-    };
-  });
+            $.jqplot(chartDivId, data, options);
+        }
+    });
--- Scripts/app/spa/taskChart/taskChart.component.js
+++ Scripts/app/spa/taskChart/taskChart.component.js
@@ -3,105 +3,20 @@
     .component('taskChart', {
         templateUrl: '/BroadridgeTestProject/Scripts/app/spa/taskChart/taskChart.template.html',
         controller: taskChartController
-    })
-    //.value('charting', {
-    //    pieChartOptions: { 
-    //        seriesDefaults: {
-    //            // Make this a pie chart.
-    //            renderer: jQuery.jqplot.PieRenderer, 
-    //            rendererOptions: {
-    //                // Put data labels on the pie slices.
-    //                // By default, labels show the percentage of the slice.
-    //                showDataLabels: true
-    //            }
-    //        }, 
-    //        legend: { show:true, location: 'e' }
-    //    }
-    //})
+    });
 
-//    .directive('uiChart', function() {
-//    return {
-//        restrict : 'EACM',
-//        template : '<div></div>',
-//        replace : true,
-//        link : function(scope, elem, attrs) {
-//            var renderChart = function() {
-//                var data = scope.$eval(attrs.uiChart);
-//                elem.html('');
-//                if (!angular.isArray(data)) {
-//                    return;
-//                }
+function taskChartController($http, $scope, chartService) {
+    $scope.taskChartData = [[['Chris', 12], ['Manuel', 9], ['Dustin', 14], ['Anu', 16], ['Vijay', 7], ['El Luchadore', 9]]];
 
-//                var opts = {};
-//                if (!angular.isUndefined(attrs.chartOptions)) {
-//                    opts = scope.$eval(attrs.chartOptions);
-//                    if (!angular.isObject(opts)) {
-//                        throw 'Invalid ui.chart options attribute';
-//                    }
-//                }
+    $scope.$watch('taskChartData', function () {
+        chartService.drawChart('chartDivId', 'Tasks by type', $scope.taskChartData);
+    });
 
-//                elem.jqplot(data, opts);
-//            };
-
-//            scope.$watch(attrs.uiChart, function() {
-//                renderChart();
-//            }, true);
-
-//            scope.$watch(attrs.chartOptions, function() {
-//                renderChart();
-//            });
-//        }
-//    };
-//})
-;
-
-function taskChartController($http, $scope) {
-    //$scope.someData = [[
-    //  ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14], 
-    //  ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
-    //]];
-
-    //$scope.myChartOpts = charting.pieChartOptions;
-
-    //pie chart data - scope
-    $scope.someData = [[['Chris', 12], ['Manuel', 9], ['Dustin', 14], ['Anu', 16], ['Vijay', 7], ['El Luchadore', 9]]];
-
-
-    // pie chart options - scope
-    $scope.myChartOpts = {
-        seriesDefaults : {
-            // use the pie chart renderer
-            renderer : jQuery.jqplot.PieRenderer,
-            rendererOptions : {
-                // Put data labels on the pie slices.
-                // By default, labels show the percentage of the slice.
-                showDataLabels : true
-            }
-        },
-        legend : {
-            show : true,
-            location : 's'
-        }
+    $scope.drawChart = function() {        
+        chartService.drawChart('chartDivId', 'Tasks by type', $scope.taskChartData);
     };
 
-
-    $scope.drawChart = function () {
-        var slice_1 = ['North America', 150];
-		var slice_2 = ['Europe', 50];
-		var series = [slice_1, slice_2];
-		var data = [series];
-		 
-		var options = {
-		title: 'Sales by Region',
-		seriesDefaults: {
-			renderer: jQuery.jqplot.PieRenderer,
-            rendererOptions: {
-              showDataLabels: true
-            }
-		},
-		legend: { show:true, location: 'e' }
-		};
-		   
-		$.jqplot('chartDivId', data, options);
-    }
+    $scope.changeDataChart = function() {
+        $scope.taskChartData = [[['Chris', 55], ['Manuel', 11], ['Dustin', 24]]];
+    };
 }
