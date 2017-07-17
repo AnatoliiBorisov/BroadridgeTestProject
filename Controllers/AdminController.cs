3fc4d93a7e792a9820eeaca66560cd7d2c8738c1
AnatoliiBorisov <borisov-anatolii@mail.ru>
2017-07-17 18:10:57 +0300

    implemented creating of users
    implemented creating of users

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using BroadridgeTestProject.Infrastructure.Identity;
+using BroadridgeTestProject.Models.Identity;
+using BroadridgeTestProject.Models.Web;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 
 namespace BroadridgeTestProject.Controllers
@@ -16,11 +20,50 @@ namespace BroadridgeTestProject.Controllers
             return View(UserManager.Users);
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+
+                var result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+
+            return View(model);
+        }
+
         private AppUserManager UserManager
         {
             get
             {
                 return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
             }
         }
     }
